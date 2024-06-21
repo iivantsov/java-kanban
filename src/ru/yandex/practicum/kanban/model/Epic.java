19482f712500bd776ebd9db2f432d90c76873063
iivantsov <iiv.88@yandex.ru>
2024-06-21 20:33:25 +0300

    Sprint-7 fromString() added for Task,Subtask,Epic

--- src/ru/yandex/practicum/kanban/model/Epic.java
+++ src/ru/yandex/practicum/kanban/model/Epic.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Epic extends Task {
+    private static final int NUMBER_OF_SUBTASKS_INDEX = 5;
     private final List<Integer> subtaskIDs;
 
     public Epic(String name, String description) {
@@ -41,5 +42,31 @@ public class Epic extends Task {
         }
 
         return builder.toString();
+    }
+
+    @Override
+    public Epic fromString(String epicAsString) {
+        String[] fields = epicAsString.split(DELIMITER);
+
+        Integer id = Integer.parseInt(fields[ID_INDEX]);
+        TaskTypes type = TaskTypes.valueOf(fields[TYPE_INDEX]);
+        String name = fields[NAME_INDEX];
+        TaskStatus status = TaskStatus.valueOf(fields[STATUS_INDEX]);
+        String description = fields[DESCRIPTION_INDEX];
+        int numberOfSubtasks = Integer.parseInt(fields[NUMBER_OF_SUBTASKS_INDEX]);
+
+        Epic epic = new Epic(name, description);
+        epic.setId(id);
+        epic.setStatus(status);
+        epic.setType(type);
+
+        for (int subtaskIndex = 0; subtaskIndex < numberOfSubtasks; ++subtaskIndex)
+        {
+            int parseIndex = subtaskIndex + NUMBER_OF_SUBTASKS_INDEX;
+            Integer subtaskID = Integer.parseInt(fields[parseIndex]);
+            epic.addSubtaskID(subtaskID);
+        }
+
+        return epic;
     }
 }
