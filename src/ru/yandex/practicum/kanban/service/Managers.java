37bfe00361a888e13ab51407fc3ab83c309d9e90
iivantsov <iiv.88@yandex.ru>
2024-06-23 16:25:54 +0300

    Sprint-7, Review-1 resources directory moved to java-kanban directory

--- src/ru/yandex/practicum/kanban/service/Managers.java
+++ src/ru/yandex/practicum/kanban/service/Managers.java
@@ -4,8 +4,10 @@ import ru.yandex.practicum.kanban.service.api.*;
 import ru.yandex.practicum.kanban.service.impl.*;
 
 public class Managers {
+    private static final String FILE_NAME = "data.csv";
+
     public static TaskManager getDefault() {
-        return new FileBackedTaskManager("data.csv");
+        return new FileBackedTaskManager(FILE_NAME);
     }
 
     public static HistoryManager getDefaultHistory() {
