eeb4b841963e580655c9153372c3efb75eba24bf
iivantsov <iiv.88@yandex.ru>
2024-07-19 02:15:45 +0300

    Sprint-9 PrioritizedTasksHttpHandler and HistoryHttpHandler implemented

--- src/ru/yandex/practicum/kanban/service/impl/httpserver/EpicsHttpHandler.java
+++ src/ru/yandex/practicum/kanban/service/impl/httpserver/EpicsHttpHandler.java
@@ -1,19 +1,29 @@
 package ru.yandex.practicum.kanban.service.impl.httpserver;
 
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import ru.yandex.practicum.kanban.service.api.TaskManager;
 
 import java.io.IOException;
+import java.util.Optional;
 
-public class EpicsHttpHandler extends BaseHttpHandler implements HttpHandler {
+public class EpicsHttpHandler extends BaseHttpHandler {
 
     public EpicsHttpHandler(TaskManager taskManager) {
         super(taskManager);
     }
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    protected void handleGetRequest(HttpExchange httpExchange, Optional<Integer> maybeId) throws IOException {
+
+    }
+
+    @Override
+    protected void handlePostRequest(HttpExchange httpExchange, Optional<Integer> maybeId) throws IOException {
+
+    }
+
+    @Override
+    protected void handleDeleteRequest(HttpExchange httpExchange, Optional<Integer> maybeId) throws IOException {
 
     }
 }
